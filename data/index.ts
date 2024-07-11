2cc69662e18e493fd8d3f9778e8d5ab460490a37
daksh3003 <dakshpandey3003@gmail.com>
2024-07-12 00:01:07 +0530

    testimonials section completed and logos and svgs of the companies created

--- data/index.ts
+++ data/index.ts
@@ -140,33 +140,33 @@ export const testimonials = [
 export const companies = [
   {
     id: 1,
-    name: "cloudinary",
-    img: "/cloud.svg",
-    nameImg: "/cloudName.svg",
+    name: "EdunetFoundation",
+    img: "/edunetlogo.svg",
+    nameImg: "/edunet.svg",
   },
   {
     id: 2,
-    name: "appwrite",
-    img: "/app.svg",
-    nameImg: "/appName.svg",
+    name: "GoldmanSachs",
+    img: "/goldmanLogo.svg",
+    nameImg: "/goldman.svg",
   },
   {
     id: 3,
-    name: "HOSTINGER",
-    img: "/host.svg",
-    nameImg: "/hostName.svg",
+    name: "DevSkillHub",
+    img: "/devskillhublogo.svg",
+    nameImg: "/Devskillhub.svg",
   },
   {
     id: 4,
-    name: "stream",
-    img: "/s.svg",
-    nameImg: "/streamName.svg",
+    name: "JivanHealth",
+    img: "/jivanlogo.svg",
+    nameImg: "/jivan.svg",
   },
   {
     id: 5,
-    name: "docker.",
-    img: "/dock.svg",
-    nameImg: "/dockerName.svg",
+    name: "TimechainLabs",
+    img: "/timechainlogo.svg",
+    nameImg: "/timechain.svg",
   },
 ];
 
